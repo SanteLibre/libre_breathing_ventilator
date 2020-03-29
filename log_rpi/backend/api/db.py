71f5fef269644f08f90b1eecc5bb48ab2de433d6
Annaelle <annaelle.duff@gmail.com>
2020-03-29 16:40:50 -0400

    api: filtering results per date (pretty loose filtering) + no get param: old code returning sim data

--- log_rpi/backend/api/db.py
+++ log_rpi/backend/api/db.py
@@ -10,3 +10,6 @@ class VentilatorData(db.Model):
     __tablename__ = 'ventilator_data'
     date = db.Column(db.INT(), unique=True, primary_key=True)  # format yyyymmddhhmmss
     values = db.Column(db.String())  # comma separated floats
+
+    def to_dict(self):
+        return {'date': self.date, 'values': self.values}
